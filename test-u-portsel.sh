87b2c6a60dbb015c32eccd95d334b852a9a9212d
fughilli <kbalke@ucla.edu>
2015-03-12 14:42:58 -0700

    Added a test script to check the behavior of the parallel upload port assignment scheme

---
+++ test-u-portsel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+START_PORT=11113
+END_PORT=13000
+
+for portnum in $(seq $START_PORT $END_PORT)
+do
+cat /dev/urandom | tr -dC "[:alnum:]" | head -c 100 | telnet 127.0.0.1 $portnum
+sleep 0.01
+done
