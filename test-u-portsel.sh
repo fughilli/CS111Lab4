b3d280a69a1802cab48843a24dc75a641545c155
fughilli <kbalke@ucla.edu>
2015-03-13 05:06:26 -0700

    Changed test script to connect rapidly to the same peer

--- test-u-portsel.sh
+++ test-u-portsel.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-START_PORT=11113
+START_PORT=11112
 END_PORT=13000
 
-for portnum in $(seq $START_PORT $END_PORT)
+#for portnum in $(seq $START_PORT $END_PORT)
+#do
+#cat /dev/urandom | tr -dC "[:alnum:]" | head -c 10 | telnet 127.0.0.1 $portnum
+##sleep 0.01
+#done
+
+for i in $(seq 100)
 do
-cat /dev/urandom | tr -dC "[:alnum:]" | head -c 10 | telnet 127.0.0.1 $portnum
-#sleep 0.01
+echo "GET cat1.jpg OSP2P" | telnet 127.0.0.1 $START_PORT 1>/dev/null 2>&1 &
+sleep 0.0001
 done
+
+#sleep 10
