86f427e6b13ff986641bd99885739e94ba2c8509
fughilli <kbalke@ucla.edu>
2015-03-12 18:32:44 -0700

    Removed delay in test script

--- test-u-portsel.sh
+++ test-u-portsel.sh
@@ -5,6 +5,6 @@ END_PORT=13000
 
 for portnum in $(seq $START_PORT $END_PORT)
 do
-cat /dev/urandom | tr -dC "[:alnum:]" | head -c 100 | telnet 127.0.0.1 $portnum
-sleep 0.01
+cat /dev/urandom | tr -dC "[:alnum:]" | head -c 10 | telnet 127.0.0.1 $portnum
+#sleep 0.01
 done
